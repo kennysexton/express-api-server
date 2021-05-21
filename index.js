eb5c558b829e5b3455d9584f159285b02b301186
kennysexton <kennysexton4@gmail.com>
2021-05-21 18:46:42 -0400

    removing cors

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middlewares
-app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
